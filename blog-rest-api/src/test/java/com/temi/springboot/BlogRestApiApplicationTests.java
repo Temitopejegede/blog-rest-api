0ea94eff0629a0a58e6ab1ef4b3697c7bd8e5d9a
Temitope Jegede <temiteejegede@gmail.com>
2022-07-01 01:07:03 -0400

    Initial commit

---
+++ blog-rest-api/src/test/java/com/temi/springboot/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
