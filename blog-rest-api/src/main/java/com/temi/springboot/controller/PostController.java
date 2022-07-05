ea751a0b9cdd081b4d2f7539bb360f261c2d884e
Temitope Jegede <temiteejegede@gmail.com>
2022-07-05 13:58:55 -0400

    added post mapping annotation

--- blog-rest-api/src/main/java/com/temi/springboot/controller/PostController.java
+++ blog-rest-api/src/main/java/com/temi/springboot/controller/PostController.java
@@ -4,6 +4,7 @@ import com.temi.springboot.payload.PostDto;
 import com.temi.springboot.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
