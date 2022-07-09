8845cb5f1f7e998a621b47bfb6c98565286bb56c
temitopejegede <temiteejegede@gmail.com>
2022-07-09 00:06:11 -0400

    added update post signature and method body

--- blog-rest-api/src/main/java/com/temi/springboot/service/impl/PostServiceImpl.java
+++ blog-rest-api/src/main/java/com/temi/springboot/service/impl/PostServiceImpl.java
@@ -45,6 +45,19 @@ public class PostServiceImpl implements PostService {
         return mapToDTO(post);
     }
 
+    @Override
+    public PostDto updatePost(PostDto postDto, Long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+
+        return mapToDTO(updatePost);
+    }
+
     //helper method to convert entity to dto
     private PostDto mapToDTO(Post post){
         PostDto postDto = new PostDto();
