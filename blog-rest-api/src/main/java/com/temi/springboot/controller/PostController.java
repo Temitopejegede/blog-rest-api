55e32426f403a5e64e831530ac638df0510624a6
temitopejegede <temiteejegede@gmail.com>
2022-07-09 23:59:06 -0400

    refactored code to keep constants in different class

--- blog-rest-api/src/main/java/com/temi/springboot/controller/PostController.java
+++ blog-rest-api/src/main/java/com/temi/springboot/controller/PostController.java
@@ -3,6 +3,7 @@ package com.temi.springboot.controller;
 import com.temi.springboot.payload.PostDto;
 import com.temi.springboot.payload.PostResponse;
 import com.temi.springboot.service.PostService;
+import com.temi.springboot.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +28,10 @@ public class PostController {
     //get all posts rest api
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value= "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "dir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value= "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "dir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ){
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
