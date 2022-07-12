ca00e94ceebed97a072874ecd74b4070af065263
temitopejegede <temiteejegede@gmail.com>
2022-07-12 00:59:25 -0400

    custom exception that can be used anywhere

---
+++ blog-rest-api/src/main/java/com/temi/springboot/exception/BlogApiException.java
@@ -0,0 +1,24 @@
+package com.temi.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BlogApiException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    public BlogApiException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
