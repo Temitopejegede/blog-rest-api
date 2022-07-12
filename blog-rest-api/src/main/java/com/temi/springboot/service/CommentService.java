ac842b35721f672ee52ee983e7ac0ed6adb490e4
temitopejegede <temiteejegede@gmail.com>
2022-07-12 01:00:22 -0400

    added method signatures

--- blog-rest-api/src/main/java/com/temi/springboot/service/CommentService.java
+++ blog-rest-api/src/main/java/com/temi/springboot/service/CommentService.java
@@ -1,7 +1,13 @@
 package com.temi.springboot.service;
 
+import com.temi.springboot.entity.Comment;
 import com.temi.springboot.payload.CommentDto;
+
+import java.util.List;
 
 public interface CommentService {
     CommentDto creteComment(long postId, CommentDto commentDto);
+    List<CommentDto> getCommentByPostId(Long postId);
+
+    CommentDto getCommentById(Long postId, Long commentId);
 }
