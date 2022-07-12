034db57739b19329ec3b0973b86271ce4a1579c4
temitopejegede <temiteejegede@gmail.com>
2022-07-12 00:59:41 -0400

    added more api s

--- blog-rest-api/src/main/java/com/temi/springboot/controller/CommentController.java
+++ blog-rest-api/src/main/java/com/temi/springboot/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.temi.springboot.controller;
 
+import com.temi.springboot.entity.Comment;
 import com.temi.springboot.payload.CommentDto;
 import com.temi.springboot.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -20,5 +23,17 @@ public class CommentController {
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") Long postId,
                                                     @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.creteComment(postId, commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") Long postId){
+        return commentService.getCommentByPostId(postId);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId){
+        CommentDto commentDto = commentService.getCommentById(postId,commentId);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
     }
 }
