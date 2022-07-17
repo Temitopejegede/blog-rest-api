09716930097e9f838bebb2858d4bc6d625daac72
temitopejegede <temiteejegede@gmail.com>
2022-07-17 00:21:44 -0400

    added validation annotations

--- blog-rest-api/src/main/java/com/temi/springboot/payload/CommentDto.java
+++ blog-rest-api/src/main/java/com/temi/springboot/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.temi.springboot.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+
+    @NotEmpty()
+    @Email(message = "Please provide a valid email")
     private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "Comment body should not be less that 10 characters")
     private String body;
 }
