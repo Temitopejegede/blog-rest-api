37ea1afaf5990dc6c362c9dd8d6e51aa958bf37a
temitopejegede <temiteejegede@gmail.com>
2022-07-16 23:20:13 -0400

    handles global exceptions

--- blog-rest-api/src/main/java/com/temi/springboot/exception/GlobalExceptionHandler.java
+++ blog-rest-api/src/main/java/com/temi/springboot/exception/GlobalExceptionHandler.java
@@ -25,4 +25,11 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handelGlobalException(Exception exception,
+                                                                        WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
