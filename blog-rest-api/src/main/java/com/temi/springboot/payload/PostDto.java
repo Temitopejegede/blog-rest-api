93452ddc4c5df244b58631c74725bbbe90d15e26
temitopejegede <temiteejegede@gmail.com>
2022-07-16 23:51:16 -0400

    added validation constraints

--- blog-rest-api/src/main/java/com/temi/springboot/payload/PostDto.java
+++ blog-rest-api/src/main/java/com/temi/springboot/payload/PostDto.java
@@ -2,13 +2,27 @@ package com.temi.springboot.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private Long id;
+
+    //title should not be empty or null
+    //title should have at least two characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have a least two characters")
     private String title;
+
+    //Post description should not be null or empty
+    //must have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
