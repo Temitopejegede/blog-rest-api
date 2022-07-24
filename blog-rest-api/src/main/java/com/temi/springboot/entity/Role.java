d53cca52c10d28a4a3d24d98ca68914ceb9d4a87
temitopejegede <temiteejegede@gmail.com>
2022-07-24 00:47:38 -0400

    correction from earlier commit

--- blog-rest-api/src/main/java/com/temi/springboot/entity/Role.java
+++ blog-rest-api/src/main/java/com/temi/springboot/entity/Role.java
@@ -21,9 +21,5 @@ public class Role {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
-    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Set<Role> roles;
+
 }
--- blog-rest-api/src/main/java/com/temi/springboot/entity/User.java
+++ blog-rest-api/src/main/java/com/temi/springboot/entity/User.java
@@ -3,6 +3,7 @@ package com.temi.springboot.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
@@ -17,5 +18,11 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 
 }
