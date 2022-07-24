5b559a98445ce45613c4c90c6725d4e721276bc3
temitopejegede <temiteejegede@gmail.com>
2022-07-23 22:37:21 -0400

    added two authorized users

--- blog-rest-api/src/main/java/com/temi/springboot/config/SecurityConfig.java
+++ blog-rest-api/src/main/java/com/temi/springboot/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.temi.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -14,9 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails temi = User.builder().username("temi").password("password").roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password("admin").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(temi, admin);
+
     }
 }
