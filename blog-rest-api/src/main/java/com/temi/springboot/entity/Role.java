56f43b65a3b3e57e52792021affec1bb111b7c46
temitopejegede <temiteejegede@gmail.com>
2022-07-24 00:14:43 -0400

    creates a sql table holds the roles of each user. many to many mapping

---
+++ blog-rest-api/src/main/java/com/temi/springboot/entity/Role.java
@@ -0,0 +1,29 @@
+package com.temi.springboot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+}
